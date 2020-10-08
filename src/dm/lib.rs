3113dab01041ed7601eb4b150a05bf24a15c9d59
Aspen <luxx4x@protonmail.com>
2020-10-08 18:12:48 -0400

    refactor: use `phf` for signatures

--- src/dm/lib.rs
+++ src/dm/lib.rs
@@ -23,6 +23,19 @@ fn random_string(n: usize) -> String {
 	thread_rng().sample_iter(&Alphanumeric).take(n).collect()
 }
 
+static SIGNATURES: phf::Map<&'static str, &'static [u8]> = phf::phf_map! {
+	"string_table" => b"\xA1????\x8B\x04?\x85\xC0\x0F\x84????\x80\x3D????\x00\x8B\x18",
+	"get_proc_array_entry" => b"\xE8????\x8B\xC8\x8D\x45?\x6A\x01\x50\xFF\x76?\x8A\x46?\xFF\x76?\xFE\xC0",
+	"get_string_id" => b"\x55\x8B\xEC\x8B\x45?\x83\xEC?\x53\x56\x8B\x35",
+	"call_proc_by_id" => b"\x55\x8B\xEC\x81\xEC????\xA1????\x33\xC5\x89\x45?\x8B\x55?\x8B\x45",
+	"get_variable" => b"\x55\x8B\xEC\x8B\x4D?\x0F\xB6\xC1\x48\x83\xF8?\x0F\x87????\x0F\xB6\x80????\xFF\x24\x85????\xFF\x75?\xFF\x75?\xE8",
+	"set_variable" => b"\x55\x8B\xEC\x8B\x4D\x08\x0F\xB6\xC1\x48\x57\x8B\x7D\x10\x83\xF8\x53\x0F?????\x0F\xB6\x80????\xFF\x24\x85????\xFF\x75\x18\xFF\x75\x14\x57\xFF\x75\x0C\xE8????\x83\xC4\x10\x5F\x5D\xC3",
+	"get_string_table_entry" => b"\x55\x8B\xEC\x8B\x4D\x08\x3B\x0D????\x73\x10\xA1",
+	"call_datum_proc_by_name" => b"\x55\x8B\xEC\x83\xEC\x0C\x53\x8B\x5D\x10\x8D\x45\xFF\x56\x8B\x75\x14\x57\x6A\x01\x50\xFF\x75\x1C\xC6\x45\xFF\x00\xFF\x75\x18\x6A\x00\x56\x53",
+	"dec_ref_count" => b"\x3D????\x74\x14\x50\xE8????\xFF\x75\x0C\xFF\x75\x08\xE8",
+	"inc_ref_count" => b"\xFF\x75\x10\xE8????\xFF\x75\x0C\x8B\xF8\xFF\x75\x08\xE8????\x57",
+};
+
 byond_ffi_fn! { auxtools_init(_input) {
 	// Already initialized. Just succeed?
 	if GLOBAL_STATE.get().is_some() {
@@ -35,7 +48,7 @@ byond_ffi_fn! { auxtools_init(_input) {
 	};
 
 	let string_table: *mut raw_types::strings::StringTable;
-	if let Some(ptr) = byondcore.find(b"\xA1????\x8B\x04?\x85\xC0\x0F\x84????\x80\x3D????\x00\x8B\x18") {
+	if let Some(ptr) = byondcore.find(SIGNATURES.get("string_table").unwrap()) {
 		unsafe {
 			// TODO: Could be nulls
 			string_table = *(ptr.offset(1) as *mut *mut raw_types::strings::StringTable);
@@ -45,7 +58,7 @@ byond_ffi_fn! { auxtools_init(_input) {
 	}
 
 	let get_proc_array_entry: raw_types::funcs::GetProcArrayEntry;
-	if let Some(ptr) = byondcore.find(b"\xE8????\x8B\xC8\x8D\x45?\x6A\x01\x50\xFF\x76?\x8A\x46?\xFF\x76?\xFE\xC0") {
+	if let Some(ptr) = byondcore.find(SIGNATURES.get("get_proc_array_entry").unwrap()) {
 		unsafe {
 			// TODO: Could be nulls
 			let offset = *(ptr.offset(1) as *const isize);
@@ -56,7 +69,7 @@ byond_ffi_fn! { auxtools_init(_input) {
 	}
 
 	let get_string_id: raw_types::funcs::GetStringId;
-		if let Some(ptr) = byondcore.find(b"\x55\x8B\xEC\x8B\x45?\x83\xEC?\x53\x56\x8B\x35") {
+	if let Some(ptr) = byondcore.find(SIGNATURES.get("get_string_id").unwrap()) {
 		unsafe {
 			// TODO: Could be nulls
 			get_string_id = std::mem::transmute(ptr as *const ());
@@ -66,7 +79,7 @@ byond_ffi_fn! { auxtools_init(_input) {
 	}
 
 	let call_proc_by_id: raw_types::funcs::CallProcById;
-	if let Some(ptr) = byondcore.find(b"\x55\x8B\xEC\x81\xEC????\xA1????\x33\xC5\x89\x45?\x8B\x55?\x8B\x45") {
+	if let Some(ptr) = byondcore.find(SIGNATURES.get("call_proc_by_id").unwrap()) {
 		unsafe {
 			// TODO: Could be nulls
 			call_proc_by_id = std::mem::transmute(ptr as *const ());
@@ -76,7 +89,7 @@ byond_ffi_fn! { auxtools_init(_input) {
 	}
 
 	let get_variable: raw_types::funcs::GetVariable;
-	if let Some(ptr) = byondcore.find(b"\x55\x8B\xEC\x8B\x4D?\x0F\xB6\xC1\x48\x83\xF8?\x0F\x87????\x0F\xB6\x80????\xFF\x24\x85????\xFF\x75?\xFF\x75?\xE8") {
+	if let Some(ptr) = byondcore.find(SIGNATURES.get("get_variable").unwrap()) {
 		unsafe {
 			// TODO: Could be nulls
 			get_variable = std::mem::transmute(ptr as *const ());
@@ -86,7 +99,7 @@ byond_ffi_fn! { auxtools_init(_input) {
 	}
 
 	let set_variable: raw_types::funcs::SetVariable;
-	if let Some(ptr) = byondcore.find(b"\x55\x8B\xEC\x8B\x4D\x08\x0F\xB6\xC1\x48\x57\x8B\x7D\x10\x83\xF8\x53\x0F?????\x0F\xB6\x80????\xFF\x24\x85????\xFF\x75\x18\xFF\x75\x14\x57\xFF\x75\x0C\xE8????\x83\xC4\x10\x5F\x5D\xC3") {
+	if let Some(ptr) = byondcore.find(SIGNATURES.get("set_variable").unwrap()) {
 		unsafe {
 			// TODO: Could be nulls
 			set_variable = std::mem::transmute(ptr as *const ());
@@ -96,7 +109,7 @@ byond_ffi_fn! { auxtools_init(_input) {
 	}
 
 	let get_string_table_entry: raw_types::funcs::GetStringTableEntry;
-	if let Some(ptr) = byondcore.find(b"\x55\x8B\xEC\x8B\x4D\x08\x3B\x0D????\x73\x10\xA1") {
+	if let Some(ptr) = byondcore.find(SIGNATURES.get("get_string_table_entry").unwrap()) {
 		unsafe {
 			// TODO: Could be nulls
 			get_string_table_entry = std::mem::transmute(ptr as *const ());
@@ -106,7 +119,7 @@ byond_ffi_fn! { auxtools_init(_input) {
 	}
 
 	let call_datum_proc_by_name: raw_types::funcs::CallDatumProcByName;
-	if let Some(ptr) = byondcore.find(b"\x55\x8B\xEC\x83\xEC\x0C\x53\x8B\x5D\x10\x8D\x45\xFF\x56\x8B\x75\x14\x57\x6A\x01\x50\xFF\x75\x1C\xC6\x45\xFF\x00\xFF\x75\x18\x6A\x00\x56\x53") {
+	if let Some(ptr) = byondcore.find(SIGNATURES.get("call_datum_proc_by_name").unwrap()) {
 		unsafe {
 			// TODO: Could be nulls
 			call_datum_proc_by_name = std::mem::transmute(ptr as *const ());
@@ -124,10 +137,10 @@ byond_ffi_fn! { auxtools_init(_input) {
 	let dec_ref_count: raw_types::funcs::DecRefCount;
 	let inc_ref_count: raw_types::funcs::IncRefCount;
 	unsafe {
-		let dec_ref_count_call: *const u8 = byondcore.find(b"\x3D????\x74\x14\x50\xE8????\xFF\x75\x0C\xFF\x75\x08\xE8").unwrap().offset(20);
+		let dec_ref_count_call: *const u8 = byondcore.find(SIGNATURES.get("dec_ref_count_call").unwrap()).unwrap().offset(20);
 		dec_ref_count = std::mem::transmute(dec_ref_count_call.offset((*(dec_ref_count_call as *const u32) + 4) as isize));
 
-		let inc_ref_count_call: *const u8 = byondcore.find(b"\xFF\x75\x10\xE8????\xFF\x75\x0C\x8B\xF8\xFF\x75\x08\xE8????\x57").unwrap().offset(17);
+		let inc_ref_count_call: *const u8 = byondcore.find(SIGNATURES.get("inc_ref_count_call").unwrap()).unwrap().offset(17);
 		inc_ref_count = std::mem::transmute(inc_ref_count_call.offset((*(inc_ref_count_call as *const u32) + 4) as isize));
 	}
 
