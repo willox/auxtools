7f129c81fa70f93b1faae55fdca46a43160322aa
1fbff5f83b23d39d38b1dfcb4cac8d9b <0a02d0db@opayq.com>
2020-10-27 19:44:05 +0100

    rewrite callbacks to be more thread safe

--- dm/src/callback.rs
+++ dm/src/callback.rs
@@ -1,8 +1,9 @@
 use crate::hook;
-use crate::raw_types;
-use crate::raw_types::values::IntoRawValue;
 use crate::runtime;
 use crate::value::Value;
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::sync::Arc;
 use std::sync::Mutex;
 
 use crate as dm;
@@ -11,6 +12,12 @@ use lazy_static::lazy_static;
 
 lazy_static! {
 	static ref READY_CALLBACKS: Mutex<Vec<FinishedCallback>> = Mutex::new(Vec::new());
+	static ref DROPPED_CALLBACKS: Mutex<Vec<usize>> = Mutex::new(Vec::new());
+}
+
+static mut NEXT_CALLBACK_ID: usize = 0;
+thread_local! {
+	static CALLBACKS: RefCell<HashMap<usize, Callback>> = RefCell::new(HashMap::new());
 }
 
 /// # Callbacks
@@ -39,30 +46,25 @@ lazy_static! {
 ///
 #[derive(Clone)]
 pub struct Callback {
-	dm_callback: crate::raw_types::values::Value,
+	dm_callback: Value,
 }
 
 pub struct FinishedCallback {
-	callback: Callback,
+	id: usize,
 	closure: Box<dyn Fn() -> Vec<Value> + Send + Sync>,
 }
 
-impl Callback {
-	pub fn new<V: AsRef<Value>>(cb: V) -> Result<Self, runtime::Runtime> {
-		// TODO: Verify this is indeed a /datum/callback
+pub struct CallbackHandle {
+	id: usize,
+}
 
-		let cb = cb.as_ref();
-
-		// TODO: LEAKS LEAKS LEAKS
-		unsafe {
-			raw_types::funcs::inc_ref_count(cb.value);
-		}
-
-		Ok(Self {
-			dm_callback: unsafe { cb.as_ref().into_raw_value() },
-		})
+impl Drop for CallbackHandle {
+	fn drop(&mut self) {
+		DROPPED_CALLBACKS.lock().unwrap().push(self.id);
 	}
+}
 
+impl CallbackHandle {
 	/// Queues this callback for execution on next timer tick.
 	pub fn invoke<F>(&self, closure: F)
 	where
@@ -70,26 +72,60 @@ impl Callback {
 		F: Fn() -> Vec<Value> + Send + Sync,
 	{
 		let finished = FinishedCallback {
-			callback: self.clone(),
+			id: self.id,
 			closure: Box::new(closure),
 		};
 		READY_CALLBACKS.lock().unwrap().push(finished);
 	}
 }
 
+impl Callback {
+	pub fn new<V: AsRef<Value>>(cb: V) -> Result<Arc<CallbackHandle>, runtime::Runtime> {
+		// TODO: Verify this is indeed a /datum/callback
+
+		let cb = cb.as_ref();
+
+		// TODO: LEAKS LEAKS LEAKS
+
+		CALLBACKS.with(|h| {
+			h.borrow_mut().insert(
+				unsafe { NEXT_CALLBACK_ID },
+				Self {
+					dm_callback: cb.as_ref().clone(),
+				},
+			)
+		});
+
+		let handle = Arc::new(CallbackHandle {
+			id: unsafe { NEXT_CALLBACK_ID },
+		});
+
+		unsafe { NEXT_CALLBACK_ID += 1 };
+
+		Ok(handle)
+	}
+}
+
 #[hook("/proc/_process_callbacks")]
 fn process_callbacks() {
-	let mut cbs = READY_CALLBACKS.lock().unwrap();
+	let mut ready_cbs = READY_CALLBACKS.lock().unwrap();
+	let mut dropped_cbs = DROPPED_CALLBACKS.lock().unwrap();
+	CALLBACKS.with(|h| {
+		let mut cbs = h.borrow_mut();
 
-	// We don't care if a callback runtimes.
-	#[allow(unused_must_use)]
-	for cb in cbs.iter() {
-		unsafe {
+		// We don't care if a callback runtimes.
+		#[allow(unused_must_use)]
+		for cb in ready_cbs.iter() {
 			let args = (cb.closure)();
-			Value::from_raw(cb.callback.dm_callback).call("Invoke", &args[..]);
-		};
-	}
-	cbs.clear();
+			let cb_val = cbs.get(&cb.id).unwrap();
+			cb_val.dm_callback.call("Invoke", &args[..]);
+		}
+		ready_cbs.clear();
 
+		for dropped in dropped_cbs.iter() {
+			cbs.remove(dropped);
+		}
+		dropped_cbs.clear();
+	});
 	Ok(Value::null())
 }
