ccbf0d4803da49992d95c49e95eaec43361e381e
William Wallace <me@wiox.me>
2020-11-03 06:42:44 +0000

    .

--- debug_server/build.rs
+++ debug_server/build.rs
@@ -1,7 +1,19 @@
+use std::env;
+
 fn main() {
-	cc::Build::new()
+	if env::var("CARGO_CFG_TARGET_FAMILY").unwrap() == "unix" {
+		cc::Build::new()
 		.file("src/execute_instruction_hook.S")
 		.file("src/execute_instruction_data.cpp")
 		.cpp(true)
 		.compile("debug-server-cpp");
+	}
+
+	if env::var("CARGO_CFG_TARGET_FAMILY").unwrap() == "windows" {
+		cc::Build::new()
+		.file("src/execute_instruction_hook.asm")
+		.file("src/execute_instruction_data.cpp")
+		.cpp(true)
+		.compile("debug-server-cpp");
+	}
 }
