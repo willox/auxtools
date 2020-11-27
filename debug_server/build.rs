469815fc3d22aebef88a5510400cdcdf8d4a9cea
Tad Hardesty <tad@platymuus.com>
2020-11-26 20:50:34 -0800

    Give assembly files descriptive names, add comments

--- debug_server/build.rs
+++ debug_server/build.rs
@@ -7,10 +7,10 @@ fn main() {
 		.file("src/execute_instruction_data.cpp");
 
 	match env::var("CARGO_CFG_TARGET_FAMILY").unwrap().as_str() {
-		"unix" => { build.file("src/execute_instruction_hook.S"); }
+		"unix" => { build.file("src/execute_instruction_hook.unix.S"); }
 		"windows" => match env::var("CARGO_CFG_TARGET_ENV").unwrap().as_str() {
-			"gnu" => { build.file("src/execute_instruction_hook_windows.S"); }
-			"msvc" => { build.file("src/execute_instruction_hook.asm"); }
+			"gnu" => { build.file("src/execute_instruction_hook.windows.S"); }
+			"msvc" => { build.file("src/execute_instruction_hook.windows.asm"); }
 			other => panic!("don't know how to build hook for family=\"windows\", env={:?}", other),
 		}
 		other => panic!("don't know how to build hook for family={:?}", other),
