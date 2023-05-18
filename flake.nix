ca2735ff32db412d00a900dc60a7eee5cce3f488
Sophie Wallace <sophie@wiox.me>
2023-05-18 11:22:25 +0100

    nix flake now builds & tests the project

--- flake.nix
+++ flake.nix
@@ -1,33 +1,28 @@
 {
   inputs = {
-	nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    fenix.url = "github:nix-community/fenix/720b54260dee864d2a21745bd2bb55223f58e297";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    fenix.url = "github:nix-community/fenix";
     fenix.inputs.nixpkgs.follows = "nixpkgs";
     naersk.url = "github:nix-community/naersk";
     naersk.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {
-    self,
-	nixpkgs,
-    fenix,
-	naersk,
-    ...
-  }:
-	let
-  		toolchain = fenix.packages.i686-linux.stable.toolchain;
-		pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgsi686Linux;
-	in
-	{
-		packages.x86_64-linux.default = (naersk.lib.x86_64-linux.override {
-			cargo = toolchain;
-			rustc = toolchain;
-		}).buildPackage {
-			src = ./.;
-			copyLibs = true;
-			nativeBuildInputs = [ pkgs.stdenv.cc pkgs.openssl_1_1 pkgs.pkg-config ];
-			CARGO_BUILD_TARGET = "i686-unknown-linux-gnu";
-          	CARGO_TARGET_I686_UNKNOWN_LINUX_GNU_LINKER = "cc";
-		};
-	};
+  outputs = { self, nixpkgs, fenix, naersk }:
+    let
+      toolchain = fenix.packages.i686-linux.stable.toolchain;
+      pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgsi686Linux;
+    in
+    {
+      packages.x86_64-linux.default = (naersk.lib.x86_64-linux.override {
+        cargo = toolchain;
+        rustc = toolchain;
+      }).buildPackage {
+        src = ./.;
+        doCheck = true;
+        copyLibs = true;
+        nativeBuildInputs = [ pkgs.stdenv.cc pkgs.openssl_1_1 pkgs.pkg-config ];
+        CARGO_BUILD_TARGET = "i686-unknown-linux-gnu";
+        CARGO_TARGET_I686_UNKNOWN_LINUX_GNU_LINKER = "${pkgs.stdenv.cc}/bin/${pkgs.stdenv.cc.targetPrefix}cc";
+      };
+    };
 }
